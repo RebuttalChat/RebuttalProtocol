074040d35c68f6768833958be187ca947e206f47
trigg <triggerhapp@gmail.com>
2026-04-11 10:30:24 +0100

    - more shared protocol

--- shared.ts
+++
@@ -1,8 +0,0 @@
-
-export type UUID = string & { __uuid: void };
-
-const uuid_regex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
-
-export function is_uuid(uuid: string): uuid is UUID {
-    return uuid_regex.test(uuid);
-}
--- v1/shared.ts
+++ v1/shared.ts
@@ -1,4 +1,12 @@
-import { UUID } from "../shared.ts";
+
+export type UUID = string & { __uuid: void };
+
+const uuid_regex = /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
+
+export function is_uuid(uuid: string): uuid is UUID {
+    return uuid_regex.test(uuid);
+}
+
 
 export interface v1_shared_message {
     roomid: string;
@@ -13,11 +21,31 @@ export interface v1_shared_message {
     type?: string;
     username: string;
 }
+
 export interface v1_shared_group_change {
     add?: string;
     remove?: string;
 }
 
+export interface v1_shared_room {
+    id: RoomUUID;
+    type: string;
+    name: string;
+    userlist: v1_shared_user[];
+}
+
+export interface v1_shared_user {
+    id: UserUUID;
+    currentRoom: string | null;
+    livestate: boolean;
+    livelabel: string;
+    name: string;
+    talking: boolean;
+    suppress: boolean;
+    status: boolean;
+    avatar: string | undefined;
+    hidden: boolean;
+}
 
 // Split types to generate TS warnings about wrong UUID type without runtime overhead
 export type UserUUID = UUID;
